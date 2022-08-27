f10ac308fc2f93f099dc79798224fb207ad7c5b6
psmever <psmever@gmail.com>
2022-08-27 11:13:08 +0900

    캐시 삭제.

--- src/routers/web/DefaultRouter.ts
+++
@@ -1,8 +0,0 @@
-import { index } from '@controller/web/defaultController';
-import { Router } from 'express';
-
-const defaultRouter = Router();
-
-defaultRouter.get('/', index);
-
-export default defaultRouter;
